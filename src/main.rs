62e4ab6eca877caa314016b7f20412fc068b698d
荀洪道 <kalxd@126.com>
2025-04-28 19:14:35 +0800

    hello world。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use ntex::web;
+
+#[web::get("/")]
+async fn index() -> impl web::Responder {
+	"hello world"
+}
+
+#[ntex::main]
+async fn main() -> std::io::Result<()> {
+	web::HttpServer::new(|| web::App::new().service(index))
+		.bind(("127.0.0.1", 8080))?
+		.run()
+		.await
 }
