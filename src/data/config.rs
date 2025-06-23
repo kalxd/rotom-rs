9b4db0ffb988fa5084c49b5df1974ef7675a6e92
荀洪道 <kalxd@126.com>
2025-06-23 18:32:49 +0800

    一点小小的代码。

--- src/data/config.rs
+++ src/data/config.rs
@@ -1,1 +1,20 @@
+use serde::Deserialize;
 
+#[derive(Debug, Deserialize)]
+pub struct ConfigDb {
+	db: String,
+	user: String,
+	host: String,
+	port: String,
+	password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+	salt: String,
+	database: ConfigDb,
+}
+
+pub fn load_config() -> Config {
+	todo!()
+}
