bde73f1db90f248d9b9e5e9fc90743d5a9a08d72
荀洪道 <kalxd@126.com>
2025-06-28 09:37:16 +0800

    添加必要的实现。

--- src/data/error.rs
+++ src/data/error.rs
@@ -1,3 +1,9 @@
+use ntex::{
+	http::{Response, StatusCode},
+	web::{self, WebResponseError},
+};
+use serde::Serialize;
+
 #[derive(Debug)]
 pub enum Error {
 	Internal(String),
@@ -13,6 +19,35 @@ macro_rules! error_fn {
 
 impl Error {
 	error_fn!(internal, Internal);
+}
+
+impl std::fmt::Display for Error {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::Internal(s) => write!(f, "内部错误：{s}"),
+		}
+	}
+}
+
+impl WebResponseError for Error {
+	fn status_code(&self) -> StatusCode {
+		match self {
+			Self::Internal(_) => StatusCode::BAD_REQUEST,
+		}
+	}
+
+	fn error_response(&self, _: &web::HttpRequest) -> Response {
+		#[derive(Serialize)]
+		struct Body {
+			msg: String,
+		}
+
+		let status_code = self.status_code();
+		let body = Body {
+			msg: self.to_string(),
+		};
+		Response::build(status_code).json(&body)
+	}
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
--- src/handler/user.rs
+++ src/handler/user.rs
@@ -1,8 +1,22 @@
-use ntex::web::{DefaultError, Responder, Scope, post, scope};
+use ntex::web::{
+	DefaultError, Scope, post, scope,
+	types::{Json, State},
+};
+use serde::Deserialize;
+
+use crate::data::{AppState, error::Result, ty::SaltPassword};
+
+#[derive(Debug, Deserialize)]
+struct LoginBody {
+	username: String,
+	password: String,
+}
 
 #[post("/login")]
-async fn login() -> impl Responder {
-	"hello world"
+async fn login(body: Json<LoginBody>, state: State<AppState>) -> Result<Json<()>> {
+	let salt = SaltPassword::new(&body.password, &state.salt);
+
+	Ok(Json(()))
 }
 
 pub fn api() -> Scope<DefaultError> {
