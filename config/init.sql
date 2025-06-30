74d8e19b5f379b5c241f622ebc13d96fa8015926
荀洪道 <kalxd@126.com>
2025-06-30 10:31:01 +0800

    添加文件表。

--- config/init.sql
+++ config/init.sql
@@ -1,6 +1,10 @@
 create extension if not exists ltree;
 create extension if not exists zhparser;
 
+create text search configuration china (parser = zhparser);
+
+ALTER TEXT SEARCH CONFIGURATION china ADD MAPPING FOR n,v,a,i,e,l WITH simple;
+
 create table if not exists "用户" (
 	编号 int primary key generated always as identity,
 	用户名 varchar(64) not null unique,
@@ -12,5 +16,21 @@ create table if not exists "用户会话" (
 	编号 int primary key generated always as identity,
 	用户编号 int not null references "用户" ("编号"),
 	令牌 uuid not null unique default gen_random_uuid(),
+	创建日期 timestamptz not null default now()
+);
+
+create table if not exists "文件" (
+	编号 int primary key generated always as identity,
+	特征 text not null unique,
+	扩展名 varchar(8),
+	创建日期 timestamptz not null default now()
+);
+
+create table if not exists "表情" (
+	编号 int primary key generated always as identity,
+	用户编号 int not null references 用户 (编号),
+	文件编号 int not null references 文件 (编号),
+	路径 ltree,
+	描述 text,
 	创建日期 timestamptz not null default now()
 );
