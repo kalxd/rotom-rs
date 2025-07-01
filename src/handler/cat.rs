f6f5d6791be4408c59cd55e9d2984e7aae29d070
荀洪道 <kalxd@126.com>
2025-07-01 11:24:36 +0800

    添加路由。

--- src/handler/cat.rs
+++ src/handler/cat.rs
@@ -1,5 +1,12 @@
-use ntex::web::{DefaultError, Scope, scope};
+use ntex::web::{DefaultError, Scope, get, scope, types::Json};
+
+use crate::data::{User, error::Result};
+
+#[get("/list")]
+async fn get_all_cat(user: User) -> Result<Json<()>> {
+	Ok(Json(()))
+}
 
 pub fn api() -> Scope<DefaultError> {
-	scope("/self/cat")
+	scope("/self/cat").service(get_all_cat)
 }
