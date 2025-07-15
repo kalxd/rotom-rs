812b8fad957af311f52ef914722a53c2f80a527b
荀洪道 <kalxd@126.com>
2025-07-15 08:52:22 +0800

    加入文件路由了。

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,8 +1,12 @@
 use ntex::web::{DefaultError, Scope, scope};
 
 mod cat;
+mod file;
 mod user;
 
 pub fn api() -> Scope<DefaultError> {
-	scope("/").service(user::api()).service(cat::api())
+	scope("/")
+		.service(user::api())
+		.service(cat::api())
+		.service(file::api())
 }
